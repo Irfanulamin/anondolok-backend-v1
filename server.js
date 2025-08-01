e281f3245abe40cc7871a77eff9144e3e9c47e25
Irfanul Amin <irfanulamin10@gmail.com>
2025-08-01 20:15:58 +0600

    test

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/api", routes);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
